292294dc1a410258e21810f324bc91a718eb8f82
Ahmed Samir <ahmed.samir@tajawal.com>
2018-07-11 19:45:53 +0400

    Singletone

---
+++ Creational/Singleton.php
@@ -0,0 +1,27 @@
+<?php
+
+class Singleton
+{
+
+    /** @var \stdClass */
+    protected static $object;
+
+
+    private function __construct()
+    {}
+
+
+    /**
+     * @return mixed
+     * @author Ahmed Samir <ahmed.samir@tajawal.com>
+     */
+    public static function createInstance(): stdClass
+    {
+        if(empty(static::$object)) {
+            static::$object = new \stdClass();
+        }
+
+        return static::$object;
+    }
+
+}
